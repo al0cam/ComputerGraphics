17dd25acc2c74d087b831d6bab05a68f12c7b5ad
Al0cam <benjaminsikac@gmail.com>
2022-11-03 11:26:19 +0100

    added fan animation

---
+++ zad/3/MT2D.js
@@ -0,0 +1,99 @@
+class MT2D {
+    matrica;
+    constructor()
+    {
+        this.identitet()
+    }
+
+    identitet(){
+        this.matrica = [
+            [1,0,0],
+            [0,1,0],
+            [0,0,1]
+        ];
+    }
+    pomakni(px, py){
+        let m = [
+            [1,0,px],
+            [0,1,py],
+            [0,0,1]
+        ];
+        this.mnozi(m);
+    }
+    skaliraj(sx, sy){
+        let m = [
+            [sx,0,0],
+            [0,sy,0],
+            [0,0,1]
+        ];
+        this.mnozi(m);
+    }
+    zrcaliNaX(){
+        let m = [
+            [1,0,0],
+            [0,-1,0],
+            [0,0,1]
+        ];
+        this.mnozi(m);
+    }
+    zrcaliNaY(){
+        let m = [
+            [-1,0,0],
+            [0,1,0],
+            [0,0,1]
+        ];
+        this.mnozi(m);
+    }
+    rotiraj(kut){
+        let radijani = kut * Math.PI / 180;
+        let m = [
+            [Math.cos(radijani),-Math.sin(radijani),0],
+            [Math.sin(radijani),Math.cos(radijani),0],
+            [0,0,1]
+        ];
+        this.mnozi(m);
+    }
+    smicanje(kut){
+        let radijani = kut * Math.PI / 180;
+        let m = [
+            [1,Math.tan(radijani),0],
+            [Math.tan(radijani),1,0],
+            [0,0,1]
+        ];
+        this.mnozi(m);
+    }
+
+    zrcaliNa(k, l) {
+        let alfa = Math.atan(k)*180/Math.PI;
+        this.pomakni(0, l);
+        this.rotiraj(alfa);
+        this.zrcaliNaX();
+        this.rotiraj(-alfa);
+        this.pomakni(0, -l);
+	}
+
+    rotirajOkoTocke(u,v,kut)
+    {
+        this.pomakni(-u,-v)
+        this.rotiraj(kut)
+        this.pomakni(u,v)
+    }
+
+    mnozi(m){
+        let rezultat = [
+            [0 ,0 ,0],
+            [0 ,0 ,0],
+            [0 ,0 ,0]
+        ];
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                for (let k = 0; k < 3; k++) {
+                    rezultat[i][j] += this.matrica[i][k] * m[k][j];
+                }
+            }
+        }
+        this.matrica = rezultat;
+    }
+
+}
